dbe14db0d2b828d5179e5ad48209f9f8142083a1
naush <lihsuan@8thlight.com>
2012-10-13 14:31:02 -0500

    Serve

--- lib/midwife/tasks.rb
+++ lib/midwife/tasks.rb
@@ -14,21 +14,52 @@ module Midwife
 
     CLEAN.include HTML + CSS
 
-    def self.build
-      desc 'Setup your environment'
-      task :setup do
-        FileUtils.mkdir_p(ASSETS) unless File.exists?(ASSETS)
-        FileUtils.mkdir_p(PUBLIC) unless File.exists?(PUBLIC)
-        current = File.expand_path(File.dirname(__FILE__))
-        FileUtils.cp(current + "/templates/" + CONFIG, CONFIG) unless File.exists?(CONFIG)
-        FileUtils.cp(current + "/templates/" + GEMFILE, GEMFILE) unless File.exists?(GEMFILE)
+    class << self
+      def build
+        desc 'Setup your environment'
+        task :setup do
+          setup
+        end
+
+        desc 'Care for your haml/scss'
+        task :care => HTML + CSS
+
+        desc 'Listen to your haml/scss'
+        task :listen do
+          listen
+        end
+
+        desc 'Serve your haml/scss'
+        task :serve do
+          serve
+        end
+
+        rule '.html' => '.haml' do |t|
+          compile("haml", t.source, t.name)
+        end
+
+        rule '.css' => '.scss' do |t|
+          compile("sass", t.source, t.name)
+        end
       end
 
-      desc 'Care for your haml/scss'
-      task :care => HTML + CSS
+      def compile(command, source, target)
+        destination = target.gsub(ASSETS, PUBLIC)
+        FileUtils.mkdir_p(File.dirname(destination))
+        sh "#{command} #{source} #{destination}"
+      end
 
-      desc 'Listen to your haml/scss'
-      task :listen do |t|
+      def setup
+        FileUtils.mkdir_p(ASSETS) unless File.exists?(ASSETS)
+        FileUtils.mkdir_p(PUBLIC) unless File.exists?(PUBLIC)
+
+        current_dir = File.expand_path(File.dirname(__FILE__))
+
+        FileUtils.cp("#{current_dir}/templates/#{CONFIG}", CONFIG) unless File.exists?(CONFIG)
+        FileUtils.cp("#{current_dir}/templates/#{GEMFILE}", GEMFILE) unless File.exists?(GEMFILE)
+      end
+
+      def listen
         Listen.to(ASSETS) do |modified, added, removed|
           (modified + added).each do |source|
             extension = File.extname(source)
@@ -49,19 +80,11 @@ module Midwife
         end
       end
 
-      rule '.html' => '.haml' do |t|
-        compile("haml", t.source, t.name)
+      def serve
+        trap('SIGINT') {}
+        fork { listen }
+        `rackup`
       end
-
-      rule '.css' => '.scss' do |t|
-        compile("sass", t.source, t.name)
-      end
-    end
-
-    def self.compile(command, source, target)
-      destination = target.gsub(ASSETS, PUBLIC)
-      FileUtils.mkdir_p(File.dirname(destination))
-      sh "#{command} #{source} #{destination}"
     end
   end
 end
