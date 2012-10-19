9072b14004e29629013afa493f73d3b5916f5430
naush <lihsuan@8thlight.com>
2012-10-19 16:14:16 -0500

    Remove render partial output

--- lib/midwife/helpers.rb
+++ lib/midwife/helpers.rb
@@ -8,7 +8,6 @@ module Midwife
       partial_file = "#{@current}/_#{partial}.haml"
       template = File.read(partial_file)
       output = Haml::Engine.new(template).render(Helpers.new(@current))
-      puts "haml: convert #{partial_file}."
       return output
     rescue Exception => e
       puts "haml: failed to convert #{partial_file}."
